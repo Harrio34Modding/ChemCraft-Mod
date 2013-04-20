7020572fc7341875f14419998ea4d18c462f78ab
harrio34 <ha.freni@me.com>
2013-04-20 10:10:20 -0700

    Added Initial Classes to the Mod

---
+++ Common/com/harrio34/chemcraft/ChemCraft.java
@@ -0,0 +1,32 @@
+package com.harrio34.chemcraft;
+
+import com.harrio34.chemcraft.lib.Reference;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+public class ChemCraft
+{
+    @PreInit
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        
+    }
+    @Init
+    public void Init(FMLInitializationEvent event)
+    {
+        
+    }
+    @PostInit
+    public void postInit(FMLPostInitializationEvent event)
+    {
+        
+    }
+    
+}
