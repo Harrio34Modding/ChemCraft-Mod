c77ae013a61eec254996e887acac8c173b93326b
harrio34 <ha.freni@me.com>
2013-05-29 11:10:22 -0600

    Added descriptions to previous nodes

--- Common/com/harrio34/chemcraft/lib/Reference.java
+++ Common/com/harrio34/chemcraft/lib/Reference.java
@@ -14,14 +14,14 @@ public class Reference
 {
     public static final String MOD_ID = "CC1";
     /**
-    * 
+    * The mod ID
     */
     public static final String MOD_NAME = "ChemCraft";
     /**
-    * 
+    * The mod's name'
     */
     public static final String VERSION = "0.0.1.0";
     /**
-    * 
+    * The version of the mod
     */
 }
